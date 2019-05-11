9f4091c81d39b16968e94f3527ad1e4a776dc530
jackwhelan <jackw97@live.ie>
2019-05-11 19:38:25 +0100

    Added GitMan module
    Incl. Get-AllRepos cmdlet
    Incl. Get-Repo cmdlet

---
+++ GitMan/Public/Get-AllRepos.ps1
@@ -0,0 +1,32 @@
+﻿<#
+ .Synopsis
+  Clone all of a users Repos.
+
+ .Description
+  Clone all git repos from a user on a server using user provided repo name, username and server.
+
+ .Parameter user
+  The user the repo belongs to.
+
+ .Parameter server
+  The server the repo is stored on.
+
+ .Example
+   # Clone all of jackwhelan's repos on GitHub.
+   Get-AllRepos jackwhelan http://github.com
+#>
+
+Function Get-AllRepos
+{
+    [CmdletBinding()]
+    param
+    (
+        $user = "jackwhelan"
+    )
+
+    (curl "https://api.github.com/users/$user/repos?page=1&per_page=100").Content |
+    ConvertFrom-Json |
+    %{ $_.clone_url } |
+    %{ & git clone $_ 2>&1 } |
+    %{ $_.ToString()}
+}
---
+++ GitMan/Public/Get-Repo.ps1
@@ -0,0 +1,41 @@
+﻿<#
+ .Synopsis
+  Clone a Repo.
+
+ .Description
+  Clone a specific git repo from a server using user provided repo name, username and server.
+
+ .Parameter repo
+  The repo to clone.
+
+ .Parameter user
+  The user the repo belongs to.
+
+ .Parameter server
+  The server the repo is stored on.
+
+ .Example
+   # Clone the Portfolio repo (using default server[github] and user[jackwhelan] values)
+   Get-Repo Portfolio
+
+ .Example
+   # Clone a specific repo using a specificied user.
+   Get-Repo hello-world-java leereilly
+
+ .Example
+   # Clone a specific repo using a specified user and a specified server.
+   Get-Repo helloword atlassian_tutorial https://bitbucket.org
+#>
+
+Function Get-Repo
+{
+    [CmdletBinding()]
+    param
+    (
+        $repo = "psModules",
+        $user = "jackwhelan",
+        $server = "http://github.com"
+    )
+
+    git clone "$($server)/$($user)/$($repo)"
+}

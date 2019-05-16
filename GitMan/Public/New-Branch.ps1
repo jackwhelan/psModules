270d73bd5bd482440855fe036a25fd10afed0892
Jack Whelan <jackw97@LIVE.IE>
2019-05-16 12:37:15 +0100

    Added New-Branch cmdlet.

---
+++ GitMan/Public/New-Branch.ps1
@@ -0,0 +1,27 @@
+﻿<#
+ .Synopsis
+  Makes a new branch.
+
+ .Description
+  Make a new branch within the git repo the user is currently in.
+
+ .Parameter name
+  The name of the new branch.
+  
+ .Example
+  # Create a new feature branch for adding a logo to a website.
+  New-Branch feature/website-logo
+#>
+
+Function New-Branch
+{
+    [CmdletBinding()]
+    param
+    (
+		[Parameter(mandatory=$true)]
+        [string] $name
+    )
+
+    git branch $name
+	git checkout $name
+}

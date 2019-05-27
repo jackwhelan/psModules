dd7dfc186773c647557b0225909ced2382de918b
Jack Whelan <jackw97@live.ie>
2019-05-27 14:35:32 +0100

    Added New-Project cmdlet (INCOMPLETE)

---
+++ JavaBuildUtils/Public/New-Project.ps1
@@ -0,0 +1,19 @@
+﻿function New-Project()
+{
+    [CmdletBinding()]
+    param
+    (
+        [String] $Name
+    )
+
+    Write-Host "Creating Project Folder..."
+    New-Item -ItemType Directory -Path $Name
+
+    Write-Host "Entering Project Folder..."
+    Set-Location $Name
+
+    Write-Host "Creating src and bin folders..."
+    New-Item -ItemType Directory -Path "src"
+    New-Item -ItemType Directory -Path "bin"
+
+}

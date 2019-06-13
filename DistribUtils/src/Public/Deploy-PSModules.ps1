208fcd1d6c08f765c6c3b247e47ae448a1416071
Jack Whelan <jackw97@live.ie>
2019-06-13 23:18:50 +0100

    Added DistribUtils Module

---
+++ DistribUtils/src/Public/Deploy-PSModules.ps1
@@ -0,0 +1,50 @@
+﻿Function Deploy-PSModules
+{
+    [CmdletBinding()]
+    param()
+	
+	Begin
+	{
+		$ModDirs = $ENV:PSModulePath -Split ";"
+	}
+	
+	Process
+	{
+		$ModDirs | ForEach-Object -Process {
+			if(Test-Path $_.FullName)
+			{
+				Write-Verbose "Module folder $_.FullName detected: Continuing..."
+			}
+			else
+			{
+				Write-Verbose "Module folder $_.FullName does not exist: Creating..."
+				New-Item -folder "$_.FullName"
+			}
+			
+			Write-Verbose "Entering $_.FullName"
+			Set-Location "$_"
+			
+			Write-Verbose "Cloning latest psModules repository..."
+			git clone http://github.com/jackwhelan/psModules.git
+			
+			Write-Verbose "Entering Repository Folder..."
+			Set-Location psModules
+			
+			Write-Verbose "Enumerating Modules in Repository..."
+			Get-ChildItem |
+			Where {$_.PsIsContainer} |
+			ForEach {
+				if(Test-Path "../$_")
+				{
+					Write-Verbose "Old Module folder detected, removing..."
+					Remove-Item "../$_"
+				}
+				Write-Verbose "Installing New Module folder..."
+				Move-Item "$_" ".." 
+			}
+		}
+	}
+	
+	End{
+	}
+}

5125158469ae5b6f3b928f2e2f2afb1d6f3921a2
Jack Whelan <jackw97@live.ie>
2019-06-21 15:59:02 +0100

    Removed broken script, modified Deploy-PSModules - still not complete

--- DistribUtils/src/Public/Deploy-PSModules.ps1
+++ DistribUtils/src/Public/Deploy-PSModules.ps1
@@ -1,49 +1,29 @@
-﻿Function Deploy-PSModules
+﻿Function Backup-PSModules
 {
     [CmdletBinding()]
-    param()
+    Param(
+		[String] $destination = "$($ENV:HomeDrive)$($ENV:HomePath)/Desktop"
+	)
 	
 	Begin {
 		$ModDirs = $ENV:PSModulePath -Split ";"
+		$modules = "Modules "
 	}
 	
 	Process {
 		$ModDirs | ForEach-Object -Process {
-			if(Test-Path "$($_)") {
-				Write-Verbose "Module folder $($_) detected: Continuing..."
-			} else {
-				Write-Verbose "Module folder $($_) does not exist: Creating..."
-				New-Item -ItemType directory "$($_)"
+			Write-Verbose "Backing up module folder $($_) to $($destination)"
+			
+			If (-not (Test-Path "$($destination)\$($modules)")) {
+				Copy-Item "$($_)" "$($destination)"
 			}
-			
-			Write-Verbose "Entering $($_)"
-			Set-Location "$($_)"
-
-            if(Test-Path "psModules") {
-                Write-Verbose "Old repository folder detected: Removing..."
-                Remove-Item "psModules" -Recurse -Force
-            }
-
-			Write-Verbose "Cloning latest psModules repository..."
-			git clone http://github.com/jackwhelan/psModules.git
-			
-			Write-Verbose "Entering Repository Folder..."
-			Set-Location psModules
-			
-			Write-Verbose "Enumerating Modules in Repository..."
-			Get-ChildItem |
-			Where {$_.PsIsContainer} |
-			ForEach {
-				if(Test-Path "../$($_)") {
-					Write-Verbose "Old Module folder detected, removing..."
-					Remove-Item "../$($_)" -Recurse -Force
+			Else {
+				While (Test-Path "$($destination)\$($modules)") {
+					$modules = modules + "I"
 				}
-				Write-Verbose "Installing New Module folder..."
-				Move-Item "$($_)" ".." 
-			}
 		}
 	}
 	
-	End{
+	End {
 	}
 }
